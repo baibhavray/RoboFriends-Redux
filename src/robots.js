20f63671bdbe867c2787201e1ac18efd7029f091
baibhavray <baibhav.ray@gmail.com>
2021-04-26 11:55:48 +0530

    Adding Fiiles

---
+++ src/robots.js
@@ -0,0 +1,105 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Baibhav Ray',
+    username: 'ray',
+    email: 'baibhav.ray@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Aman Ray',
+    username: 'aman',
+    email: 'aman@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Datatyaya Samal',
+    username: 'datatyaya',
+    email: 'datatyaya@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Adrisha Mohapatra',
+    username: 'adrisha',
+    email: 'adrisha@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Maddi Ramaiah',
+    username: 'maddi',
+    email: 'maddi@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Ashish Abhishek',
+    username: 'ashish_abhishek',
+    email: 'ashish@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Deepak Sahu',
+    username: 'deepak',
+    email: 'deepak@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Alok Kumar',
+    username: 'alok',
+    email: 'alok@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Mousumi Pany',
+    username: 'mousumi',
+    email: 'mousumi@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Biswajeet Sahoo',
+    username: 'biswajeet',
+    email: 'biswajeet@gmail.com'
+  },
+  {
+    id: 11,
+    name: 'Chinmayee Behera',
+    username: 'chinmayee',
+    email: 'chinmayee@gmail.com'
+  },
+  {
+    id: 12,
+    name: 'Vipul Kumar',
+    username: 'vipul',
+    email: 'vipul@gmail.com'
+  },
+  {
+    id: 13,
+    name: 'Dibya Jyoti',
+    username: 'dibya',
+    email: 'dibya@gmail.com'
+  },
+  {
+    id: 14,
+    name: 'Suprita Srichandan',
+    username: 'suprita',
+    email: 'suprita@gmail.com'
+  },
+  {
+    id: 15,
+    name: 'Mousumi Ray',
+    username: 'mousumi_ray',
+    email: 'mousumi_ray@gmail.com'
+  },
+  {
+    id: 16,
+    name: 'Shrabani Lenka',
+    username: 'shrabani',
+    email: 'shrabani@gmail.com'
+  },
+  {
+    id: 17,
+    name: 'Barsha Roy',
+    username: 'barsha',
+    email: 'barsha@gmail.com'
+  }
+
+];
